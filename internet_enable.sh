349e9b192acdd01d322c2586b52bcaf3fb2e4d3f
ayushjain141 <54855842+ayushjain141@users.noreply.github.com>
2021-12-29 00:02:58 +0530

    Update description in internet script.

--- internet_enable.sh
+++ internet_enable.sh
@@ -1,13 +1,12 @@
-####
-# this script alone can  
-####
+########################################################################################
+# This script alone can enable the connection to internet in Beagle Bone Black.
+# ( Tested only for internet over USB )
+#
+# Usage -
+#        1. Place the script in the root directory.
+#        2. Run with cmd : chmod +x  internet_enable.sh
+########################################################################################
 
 sudo /sbin/route add default gw 192.168.7.1
 echo "nameserver 8.8.8.8" >> /etc/resolv.conf
 ping google.com
-
-####
-# compile with cmd : chmod +x  internet_enable.sh  
-###
-
-
